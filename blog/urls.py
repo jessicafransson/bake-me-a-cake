2592fd197f5f205e2b55568206af67cdf0986399
Jessica Fransson <jessicafransson@outlook.com>
2022-11-14 16:27:31 +0000

    setup add blog post

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('createpost/', views.CreatePost.as_view(), name='createpost'),
+    path('createpost/', views.CreatePost.as_view(), name='create_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
