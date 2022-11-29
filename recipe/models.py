90217fd0d49546e7ce0e865c31357e95ebd68868
Jessica Fransson <jessicafransson@outlook.com>
2022-11-29 10:48:53 +0000

    updates to create recipe file

--- recipe/models.py
+++ recipe/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # for users added recipes
-class Recipe(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
