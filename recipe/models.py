3c5a721aba38105c06c7d9797247b1ae970fd467
Jessica Fransson <jessicafransson@outlook.com>
2022-11-29 09:40:12 +0000

    removed summernote for users added recipes

--- recipe/models.py
+++ recipe/models.py
@@ -4,13 +4,12 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# for users recipes
-class Post(models.Model):
+# for users added recipes
+class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
