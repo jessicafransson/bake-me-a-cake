f825e1b09173b8f1349bf3820e992be83e27d500
Jessica Fransson <jessicafransson@outlook.com>
2022-11-30 14:02:20 +0000

    updates to comments on views py file

--- recipe/views.py
+++ recipe/views.py
@@ -40,7 +40,7 @@ class PostDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
-        """ view comments """
+        """ view and validate comments for recipes"""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
