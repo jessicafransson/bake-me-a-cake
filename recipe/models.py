34364d5df8cdbcc83c1814f351edbd8c0eb694fd
Jessica Fransson <jessicafransson@outlook.com>
2022-12-07 15:14:06 +0000

    fixed issues with lines too long

--- recipe/models.py
+++ recipe/models.py
@@ -12,13 +12,17 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_post")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_post"
+        )
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True, null=True)
     featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='recipe_likes', blank=True
+        )
 
     class Meta:
         ordering = ['-created_on']
@@ -33,15 +37,16 @@ class Post(models.Model):
         return reverse('post_detail')
 
     def save(self, *args, **kwargs):
-        if not self.slug: 
-            self.slug = slugify(self.title) 
+        if not self.slug:
+            self.slug = slugify(self.title)
             return super().save(*args, **kwargs)
 
 
 # for comments on the recipes
 class Comment(models.Model):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
