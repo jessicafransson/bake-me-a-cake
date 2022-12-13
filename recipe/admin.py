6c50fa464d8e88d47c5e79f538f41c7bfb217ce6
Jessica Fransson <jessicafransson@outlook.com>
2022-12-13 10:57:21 +0000

    updates to the admin panel for recipe posts

--- recipe/admin.py
+++ recipe/admin.py
@@ -3,15 +3,14 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-# for managing the admin pages own posts, filtering and automatic slug for url
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """features for the admin recipe model """
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    search_fields = ['title', 'content', 'description', 'ingredients']
+    prepopulated_fields = {'slug': ('author', 'title')}
+    list_filter = ('status', 'created_on', 'author')
+    summernote_fields = ('content', 'method')
 
 
 # for managing the comments left on the posts
